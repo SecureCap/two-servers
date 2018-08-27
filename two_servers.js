80fde3f3a6b5053b47f2120d91c3b311d1826d6d
Secure Cap <Twilliams397tw@gmail.com>
2018-08-27 10:57:10 -0500

    final commit

--- two_servers.js
+++ two_servers.js
@@ -4,21 +4,21 @@ var portOne = 7000;
 var portTwo = 7500;
 
 function handleRequestOne(request, response){
-    response.end(`You're Awesome! Keep up the good Work`);
+    response.end(`You're Awesome! Keep up the good Work` + request.url);
 }
 function handleRequestTwo(request, response){
-    response.end(`You suck!!`);
+    response.end(`You suck!!` + request.url);
 }
 
 var server = http.createServer(handleRequestOne);
 var serverTwo = http.createServer(handleRequestTwo);
 
 server.listen(portOne, function(){
-    console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on http://localhost:${portOne}`);
 })
 
 serverTwo.listen(portTwo, function(){
-    console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on http://localhost:${portTwo}`);
 })
 
 
